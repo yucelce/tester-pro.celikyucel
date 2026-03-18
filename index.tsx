a69ecf7d92de7ee9ae086316c3f098935f73eb4f
yucelce <88087749+yucelce@users.noreply.github.com>
2026-03-18 10:01:39 +0300

    VERCEL analitycs

--- index.tsx
+++ index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './src/App';
+import { Analytics } from '@vercel/analytics/react'; // 1. BURAYI EKLEYİN
+
 
 const root = createRoot(document.getElementById('root')!);
-root.render(<App />);
+root.render(
+  <>
+    <App />
+    <Analytics /> {/* 2. BURAYI EKLEYİN */}
+  </>
+);
